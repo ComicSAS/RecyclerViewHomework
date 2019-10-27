f4bcb3b534164db0e12afd2944fd28196f594627
Radik <berezhnoy.rodion.andr0id@gmail.com>
2019-10-27 20:51:54 +0200

    Pagination

--- app/src/main/java/com/example/recyclerviewhomework/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewhomework/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.recyclerviewhomework;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ClipData;
 import android.os.Bundle;
 
+import com.example.recyclerviewhomework.pagenation.PaginationListener;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    ArrayList itemList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initRecyclerView();
+    }
+
+    protected void initRecyclerView() {
+        recyclerView = findViewById(R.id.item_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+        recyclerView.addOnScrollListener(new PaginationListener(layoutManager) {
+            @Override
+            protected void loadMoreItems() {
+            }
+        });
     }
 }
