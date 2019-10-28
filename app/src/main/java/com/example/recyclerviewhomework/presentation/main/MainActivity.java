c40339be17ebe80720229ffb797bbf252c4ec3ac
Sukhovolskij.Andrew <sukhovolskijandrewoid@gmail.com>
2019-10-28 12:07:37 +0200

    updated logic

--- app/src/main/java/com/example/recyclerviewhomework/presentation/main/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewhomework/presentation/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.recyclerviewhomework.presentation.main;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recyclerviewhomework.R;
+import com.example.recyclerviewhomework.adapter.UserArrayAdapter;
 import com.example.recyclerviewhomework.pagenation.PaginationListener;
 
 import java.util.ArrayList;
@@ -20,9 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
 
-    ArrayList itemList = new ArrayList<>();
+    ArrayList userList = new ArrayList<>();
 
-    String[] names = nameArray, dateOfBirth = dateOfBirthArray, images = imageArray;
+    String[] names = nameArray, birthDates = dateOfBirthArray, images = imageArray;
 
 
     @Override
@@ -33,16 +35,33 @@ public class MainActivity extends AppCompatActivity {
         initRecyclerView();
     }
 
+    private void initData() {
+        // Populating list items
+        for (int i = 0; i < 25; i++) {
+            String name = names[i];
+            String dateOfBirth = birthDates[i];
+            Uri uri = Uri.parse(images[i]);
+//            userList.add(new User(name, dateOfBirth,
+//                    "Description " + i + "\nCool Person"
+//                            + "\nGreat Guy!", uri));
+        }
+    }
+
     protected void initRecyclerView() {
+        final UserArrayAdapter userArrayAdapter = new UserArrayAdapter();
         recyclerView = findViewById(R.id.item_list);
+        // use a linear layout manager
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-
+        recyclerView.setAdapter(userArrayAdapter);
+        initData();
+        userArrayAdapter.addItems(userList);
 
         recyclerView.addOnScrollListener(new PaginationListener(layoutManager) {
             @Override
             protected void loadMoreItems() {
+                userArrayAdapter.addItems(userList);
             }
         });
     }
