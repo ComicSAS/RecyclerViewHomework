bc1487d4982c7f07971e7da1713fd79947d9e746
Radik <berezhnoy.rodion.andr0id@gmail.com>
2019-10-28 13:19:18 +0200

    User update

--- app/src/main/java/com/example/recyclerviewhomework/model/User.java
+++ app/src/main/java/com/example/recyclerviewhomework/model/User.java
@@ -1,5 +1,51 @@
 package com.example.recyclerviewhomework.model;
 
+import android.net.Uri;
+
 public class User {
-    //TODO user structure by Radik
+
+        private String name;
+        private String birthDate;
+        private String description;
+        private Uri image;
+
+    public User(String name, String birthDate, String description, Uri image){
+        this.name = name;
+        this.birthDate = birthDate;
+        this.description = description;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Uri getImage() {
+        return image;
+    }
+
+    public void setImage(Uri image) {
+        this.image = image;
+    }
+
 }
